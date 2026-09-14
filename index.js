634810909425d83dd92d1a172636a5b2d3ecc191
Subahan Pathan <subahanpathan4@gmail.com>
2026-09-14 23:32:19 +0530

    Fix Vercel Express ESM runtime

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿let appPromise;
+
+module.exports = async (req, res) => {
+  if (!appPromise) {
+    appPromise = import("./artifacts/api-server/dist/vercel.mjs");
+  }
+
+  const { default: app } = await appPromise;
+  return app(req, res);
+};
--- index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import app from "./artifacts/api-server/src/app";
-
-const server = express();
-
-server.use(app);
-
-export default server;
